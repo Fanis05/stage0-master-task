144d5b18917be8b50fa4fb97925a5dc456786767
Fanis <mrduck597@gmail.com>
2023-06-01 23:21:17 +0500

    test5

--- src/main/java/com/epam/conditions/DaysInMonth.java
+++ src/main/java/com/epam/conditions/DaysInMonth.java
@@ -3,18 +3,19 @@ package com.epam.conditions;
 public class DaysInMonth {
 
     public void printDays(int year, int month) {
-        if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month ==12){
+        if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
             System.out.println("31");
-        } else if(month == 4 || month ==6 || month==9 || month==11){
+        } else if (month == 4 || month == 6 || month == 9 || month == 11) {
             System.out.println("30");
-        } else if(month == 2){
-            if(year >=0 && ((year%4 == 0 && year%100 != 0) || (year%400 ==0))){
+        } else if (month == 2) {
+            if (year >= 0 && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))) {
                 System.out.println("29");
-            } else if(year>=0){
+            } else {
                 System.out.println("28");
             }
+        } else {
+            System.out.println("invalid date");
         }
-        System.out.println("invalid date");
     }
 
 }
